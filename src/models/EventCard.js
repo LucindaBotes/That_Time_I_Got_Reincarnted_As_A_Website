92e8794a7aa980a6a312d68b03b8570ac0655be1
LucindaBotes <lucinda.b2000@gmail.com>
2022-10-03 11:24:02 +0200

    Change card layout

--- src/models/EventCard.js
+++ src/models/EventCard.js
@@ -22,18 +22,20 @@ export class EventCard {
     return `
       <div class="col-3">
         <div class="card-deck">
-        <div class="card m-2">
-        <img src="${image}" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">${title}</h5>
-          <p class="card-text m-0">${description}</p>
-          <div class="row gx-5">
-            <p class="card-text m-0"><small class="text-muted">${date}</small></p>
-            <p class="card-text m-0"><small class="text-muted">${location}</small></p>
+          <div class="card m-2">
+          <img src="${image}" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">${title}</h5>
+            <p class="card-text m-0">${description}</p>
+            <div class="flex-row d-flex justify-content-between">
+              <p class="card-text m-0"><small class="text-muted">${date}</small></p>
+              <p class="card-text m-0"><small class="text-muted">${location}</small></p>
+            </div>
+            <div class="flex-row d-flex justify-content-between">
+              <p class="card-text m-0"><small class="text-muted">${level}</small></p>
+              <p class="card-text m-0"><small class="text-muted">${reward}</small></p>
+            </div>
           </div>
-          <p class="card-text m-0"><small class="text-muted">${level}</small></p>
-          <p class="card-text m-0"><small class="text-muted">${reward}</small></p>
-        </div>
         </div>
       </div>
     `;
